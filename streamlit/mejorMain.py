b83e8b14b90151e324d65a49afe32e38c8c9c41e
Nameless <nicola.petetta@gmail.com>
2024-07-01 15:38:30 -0300

    mejor main terminado

--- streamlit/mejorMain.py
+++ streamlit/mejorMain.py
@@ -4,24 +4,24 @@ import joblib
 import os
 from handlers.clean_igual import preprocessor
 
-# Define the paths to the joblib files
+# modelos
 LOGISTIC_MODEL_PATH = "handlers/model/logisticmodel.joblib"
 LINEAR_MODEL_PATH = "handlers/model/linealmodel.joblib"
 
 
-# Load and cache the classification model
+# Clasificacion
 @st.cache_resource(show_spinner="Loading model...")
 def load_classification_model():
     return joblib.load(LOGISTIC_MODEL_PATH)
 
 
-# Load and cache the regression model
+# Regresion
 @st.cache_resource(show_spinner="Loading model...")
 def load_regression_model():
     return joblib.load(LINEAR_MODEL_PATH)
 
 
-# Load and preprocess the dataset
+# Cargar y limpiar
 def load_and_preprocess_data():
     path = "weatherAUS.csv"
     dataframe = pd.read_csv(path, usecols=range(1, 25))
@@ -43,10 +43,10 @@ def load_and_preprocess_data():
     return (
         dataframe_display,
         df_limpio,
-    )  # Return both the original and preprocessed data
+    )
 
 
-# Main app
+# FRONT
 header = st.container()
 model_training = st.container()
 header_lineal = st.container()
@@ -56,18 +56,18 @@ with header:
     st.title("Predicción de lluvia en Australia")
     original_df, df_limpio = load_and_preprocess_data()
 
-    # Load classification model
+    # CLas
     pipeline_clas = load_classification_model()
     feature_names = pipeline_clas.named_steps["imputer"].get_feature_names_out()
 
-    # User input for classification model
+    # Input class
     columnas_numericas = list(original_df.columns[:-1])
     st.header("Ajusta los parámetros para que el modelo prediga")
     features = [
         st.slider(
             columna,
-            float(original_df[columna].min()),  # Use original data ranges
-            float(original_df[columna].max()),  # Use original data ranges
+            float(original_df[columna].min()),  # usar data original para los sliders
+            float(original_df[columna].max()),
             float(original_df[columna].mean()),
         )
         for columna in columnas_numericas
@@ -77,7 +77,7 @@ with header:
         "¿Hoy llovió?", list(raintoday_option_mapping.keys())
     )
 
-    # Prediction for classification model
+    # pjrediccion class
     all_features = features + [raintoday_option_mapping[raintoday_option]]
     datos = pd.DataFrame([all_features], columns=feature_names)
     pred_clas = pipeline_clas.predict(datos)
@@ -87,18 +87,18 @@ with header:
 with header_lineal:
     st.title("Predicción cantidad de lluvia para el día siguiente en Australia")
 
-    # Load regression model
+    # Reg
     pipeline_reg = load_regression_model()
     feature_names = pipeline_reg.named_steps["imputer"].get_feature_names_out()
 
-    # User input for regression model
+    # Input reg
     columnas_numericas = list(original_df.columns[:-1])
     st.header("Ajusta los parámetros para que el modelo prediga")
     features = [
         st.slider(
             f"{columna}_{index}",
-            float(original_df[columna].min()),  # Use original data ranges
-            float(original_df[columna].max()),  # Use original data ranges
+            float(original_df[columna].min()),  # usar data original para los sliders
+            float(original_df[columna].max()),
             float(original_df[columna].mean()),
         )
         for index, columna in enumerate(columnas_numericas)
@@ -108,7 +108,7 @@ with header_lineal:
         "¿Hoy llovió?", list(raintoday_option_mapping.keys()), key="Llovió"
     )
 
-    # Prediction for regression model
+    # Reg pred
     all_features = features + [raintoday_option_mapping[raintoday_option]]
     datos = pd.DataFrame([all_features], columns=feature_names)
     pred_clas = pipeline_reg.predict(datos)
